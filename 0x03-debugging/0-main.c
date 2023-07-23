7e3d6efe5fd1d1b902421e8de6b45e1fc5517476
Levis Munene <munenelevis101@outlook.com>
2023-07-23 02:09:39 -0700

    oxo3

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
  * main - tests function that prints if integer is positive or negative
- * Description - prints if integer is positive or negative
+ * Description - checks value of i and prints if integer is positive or negative
  * Return: 0
  */
 
