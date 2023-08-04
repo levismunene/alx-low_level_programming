df34235cad82e5cf3f3aeab084e4d5ac1eb3f459
Levis Munene <munenelevis101@outlook.com>
2023-08-04 03:03:01 -0700

    0x09

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
- 
+
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
@@ -16,5 +16,5 @@ int main(int argc, char *argv[])
 	{
 		printf("%s\n", argv[i]);
 	}
-	return (0); 
+	return (0);
 }
