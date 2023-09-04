fd6c0ff68601e91857ff57fad0e0942c96f1000c
Levis Munene <munenelevis101@outlook.com>
2023-09-04 06:45:40 -0700

    0x15

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -56,7 +56,7 @@ void print_magic(unsigned char *e_ident)
 	{
 		printf("%02x", e_ident[index]);
 
-		if (index == EI_NIDENT -1)
+		if (index == EI_NIDENT - 1)
 
 			printf("\n");
 		else
@@ -116,7 +116,7 @@ void print_data(unsigned char *e_ident)
  * print_version - Prints the version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
  */
- void print_version(unsigned char *e_ident)
+void print_version(unsigned char *e_ident)
 {
 	printf("  Version:                         %d",
 			e_ident[EI_VERSION]);
@@ -248,22 +248,6 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
  * close_elf - Closes an ELF file.
  * @elf: The file descriptor of the ELF file.
  *
- * Description: If the cannot be closed - exit code 98.
- */
-void close_elf(int elf)
-{
-	if (close(elf) == -1)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't close fd %d\n", elf);
-		exit(98);
-	}
-}
-
-/**
- * close_elf - Closes an ELF file.
- * @elf: The file descriptor of the ELF file.
- *
  * Description: If the file cannot be closed - exit code 98.
  */
 void close_elf(int elf)
@@ -314,16 +298,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	check_elf(header->e_indent);
+	check_elf(header->e_ident);
 	printf("ELF Header:\n");
-	print_magic(header->e_indent);
-	print_class(header->e_indent);
-	print_data(header->e_indent);
-	print_version(header->e_indent);
-	print_osabi(header->e_indent);
-	print_abi(header->e_indent);
-	print_type(header->e_type, header->e_indent);
-	print_entry(header->e_entry, header->e_entry);
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(o);
